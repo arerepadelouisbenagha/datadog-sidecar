cf3c01b8158ad55458911647e4ed675e7b0ce83a
Louis Benagha <aredelulu@gmail.com>
2023-03-26 12:30:22 -0500

    changes to shell script

--- scripts/deploy-to-target.sh
+++ scripts/deploy-to-target.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-
 
 # Get variables from environment
 DOCKER_USERNAME="${DOCKER_USERNAME:-dockerhub-username}"
@@ -17,32 +13,6 @@ if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $DOCKER_IMAGE || -z $SERV
     echo "Error: One or more required variables are not set."
     exit 1
 fi
-
-# Check if SSH command is available
-if ! command -v ssh &> /dev/null; then
-    echo "Error: SSH command not found. Please install OpenSSH client."
-    exit 1
-fi
-
-# Check if docker and docker-compose are installed on the remote server, and install them if necessary
-ssh -o StrictHostKeyChecking=no ${SERVER_USERNAME}@${SERVER_HOST} "
-  if ! command -v docker &> /dev/null; then
-    echo 'Docker not found, installing...'
-    sudo apt-get update
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable\"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
-  fi
-
-  if ! command -v docker-compose &> /dev/null; then
-    echo 'Docker Compose not found, installing...'
-    sudo apt-get update
-    sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-  fi
-"
 
 # Log in to DockerHub, pull the image, and run docker-compose on the target server
 ssh -o StrictHostKeyChecking=no ${SERVER_USERNAME}@${SERVER_HOST} "
