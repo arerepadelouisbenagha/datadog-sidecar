03283a0d3e0e8d439721284a07003331d977feeb
Louis Benagha <aredelulu@gmail.com>
2023-03-26 16:11:17 -0500

    update to shell script

--- scripts/deploy-to-target.sh
+++ scripts/deploy-to-target.sh
@@ -11,7 +11,9 @@ datadog_api_key="${datadog_api_key:-datadog_api_key}"
 sudo usermod -aG docker $USER
 
 sed "s/${datadog_api_key}/$datadog_api_key/g" datadog-sidecar/datadog-sidecar/datadog.yaml
+sed 's/\${datadog_api_key}/$datadog_api_key/' datadog-sidecar/docker-compose.yml
 
+sed 's/\${datadog_api_key}/$datadog_api_key/' docker-compose.yml
 # Check if variables are set
 if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $SERVER_USERNAME || -z $SERVER_HOST ]]; then
     echo "Error: One or more required variables are not set."
